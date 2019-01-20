93a4bb422efceec842584c9438cc3f361766d174
Galina Avdeeva <cornhedgehog@gmail.com>
2019-01-20 18:48:56 +0200

    minor bug fix
    error placeholder invisible if there is no error; fixed error if one ip is being processed

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var err = document.getElementById("error");
+err.hidden = true;
 var ipListArea = document.getElementById("ips");
 var resultArea = document.getElementById("result");
 var resultNumber = 0;
+var ipsArr = [];
 
 function sendRequest() {
     var inputArray = ipListArea.value.split("\n");
-    resultArea.value = "number;ip;country\n";
 
     // From this service you can get information not more for 100 IPs for one request
     // Split IPs array to subarrays with 100 elements
@@ -22,12 +23,20 @@ function get(ips) {
     request.onload = function () {
 
         var data = JSON.parse(this.response);
+        if (Array.isArray(data)) {
+            ipsArr = data;
+        } else {
+            ipsArr.push(data);
+        }
         if (request.status >= 200 && request.status < 400) {
-            data.forEach(ipData => {
+            resultArea.value = "";
+            ipsArr.forEach(ipData => {
                 resultArea.value += ++resultNumber + "; " + ipData.ip + "; " + (ipData.country == null ? "-" : ipData.country.name_en) + '\n';
             });
+            ipsArr = [];
         }
         else {
+            err.hidden = false;
             const errorMessage = document.createElement('marquee');
             errorMessage.textContent = `Error`;
             err.appendChild(errorMessage);
